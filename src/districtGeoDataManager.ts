ae4f24d69542b6c0c9a7852f1efa6a9da70935ae
sudeepbogati7 <sudeepbogati977@gmail.com>
2025-03-30 23:21:15 +0545

    minocr change and bug fixes

--- src/districtGeoDataManager.ts
+++ src/districtGeoDataManager.ts
@@ -24,3 +24,6 @@ class DistrictGeoDataManager implements IDistrictGeoDataManager {
 const districtGeoDataManager = new DistrictGeoDataManager();
 
 export default districtGeoDataManager;
+
+
+
--- src/mapProviders.ts
+++ src/mapProviders.ts
@@ -1,17 +1,17 @@
 interface MapProviderConfig {
-    url: string;
-    attribution: string;
+  url: string;
+  attribution: string;
+}
+
+export const mapProviders: { [key: string]: MapProviderConfig } = {
+  osm: {
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  },
+  stamenTerrain: {
+    url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>'
   }
-  
-  export const mapProviders: { [key: string]: MapProviderConfig } = {
-    osm: {
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    },
-    stamenTerrain: {
-      url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
-      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>'
-    }
-  };
-  
-  export type MapProvider = keyof typeof mapProviders;
+};
+
+export type MapProvider = keyof typeof mapProviders;
